39af292f67b206de7118c550e08129b582a91443
taufik234 <48504038+taufik234@users.noreply.github.com>
2023-10-27 14:15:25 +0700

    Update Home.jsx

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,8 +1,13 @@
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+
 //HOME PAGE
 export default function Home() {
     return (
       <div>
+          <Navbar />
         <h3>INI HOMEPAGE ROUTE</h3>
+          <Footer />
       </div>
     );
   }
